2375b9b5fc341d9feb5947500b9a5ee4a6da4544
valterUo <valter.uotila@gmail.com>
2020-02-16 19:29:06 +0200

    Updates

---
+++ precache-manifest.94154926785f3658795229d2deb45f1f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "51f0f7dd9303a1bbc33cb36953edc9bf",
+    "url": "/valteruo/index.html"
+  },
+  {
+    "revision": "ae5000d384b59e9465f7",
+    "url": "/valteruo/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "29e3b7b23ff96775274a",
+    "url": "/valteruo/static/js/2.eb42f934.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/valteruo/static/js/2.eb42f934.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ae5000d384b59e9465f7",
+    "url": "/valteruo/static/js/main.292d0126.chunk.js"
+  },
+  {
+    "revision": "25673565505b247ce048",
+    "url": "/valteruo/static/js/runtime-main.60c21f07.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/valteruo/static/media/logo.5d5d9eef.svg"
+  }
+]);
